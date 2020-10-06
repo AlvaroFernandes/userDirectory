cf97bdc765481aae51dd2b6566d2c4641bde60e1
Alvaro Fernandes <fernandes.alvaro@gmail.com>
2020-10-06 21:27:27 +1100

    Updates

---
+++ precache-manifest.94f9cbb3f9a73933ae702edd21bfbeb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d008786bfa8c89d502a460b5d61557",
+    "url": "/userDirectory/index.html"
+  },
+  {
+    "revision": "9f640f963762cb4f4eca",
+    "url": "/userDirectory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9d9b7988f3ec9f8cabb5",
+    "url": "/userDirectory/static/css/main.b584eadd.chunk.css"
+  },
+  {
+    "revision": "9f640f963762cb4f4eca",
+    "url": "/userDirectory/static/js/2.02a0c0cb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/userDirectory/static/js/2.02a0c0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d9b7988f3ec9f8cabb5",
+    "url": "/userDirectory/static/js/main.706cc7ca.chunk.js"
+  },
+  {
+    "revision": "8a6c1d8d2c0a9f9483bf",
+    "url": "/userDirectory/static/js/runtime-main.08cdabe6.js"
+  }
+]);
